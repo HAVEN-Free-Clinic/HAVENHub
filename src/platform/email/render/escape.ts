75e8993148b77b04568b26c956e89a546479cae5
Jack C <j.carney@bulldogs.yale.edu>
2026-06-08 18:34:05 -0400

    feat(email): shared HTML-escape util for render engine

---
+++ src/platform/email/render/escape.ts
@@ -0,0 +1,9 @@
+/** Escape user-supplied values before interpolating into HTML. */
+export function esc(value: string): string {
+  return value
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
